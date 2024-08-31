97d419a987081f90c8492164468d13c27c040ef2
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-09-01 00:45:48 +0530

    ✅

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function palindrome() {
     }
     let isPalindrome = true;
     let processedStr = textInput.value.replace(/[\W]/g, "");
-
+    /* this is very nice algo ig checking letters from both start and end if they have same value and type then it's a palidrome **/ 
     for (let i = 0; i < processedStr.length / 2; i++ ) {
         if (processedStr[i] !== processedStr[processedStr.length - 1 - i]) {
             isPalindrome = false;
@@ -22,8 +22,10 @@ function palindrome() {
     }
     if (isPalindrome) {
         hint.innerHTML = `${textInput.value} is a palindrome💡`;
+        hint.style.textAlign = "center";
     } else {
         hint.innerHTML = `${textInput.value} is not a palindrome💡`;
+        hint.style.textAlign = "center";
     }
 }
 
